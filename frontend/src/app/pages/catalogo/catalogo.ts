9aad494f67d94790942fa045756e273fe9fd3c8f
nnova7 <gina.hada@gmail.com>
2026-04-08 21:41:56 -0600

    Se agrego catalogo ts

--- frontend/src/app/pages/catalogo/catalogo.ts
+++ frontend/src/app/pages/catalogo/catalogo.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+import { LibrosService, Libro } from '../../services/libros';
+import { CarritoService } from '../../services/carrito';
 
 @Component({
   selector: 'app-catalogo',
-  imports: [],
+  imports: [RouterLink, CurrencyPipe],
   templateUrl: './catalogo.html',
-  styleUrl: './catalogo.css',
+  styleUrl: './catalogo.css'
 })
-export class Catalogo {
+export class Catalogo implements OnInit {
+  libros = signal<Libro[]>([]);
+  cargando = signal(true);
+  error = signal('');
 
+  librosService = inject(LibrosService);
+  carritoService = inject(CarritoService);
+
+  ngOnInit() {
+    this.librosService.getLibros().subscribe({
+      next: (data) => {
+        this.libros.set(data);
+        this.cargando.set(false);
+      },
+      error: () => {
+        this.error.set('No se pudieron cargar los libros.');
+        this.cargando.set(false);
+      }
+    });
+  }
+
+  agregarAlCarrito(libro: Libro) {
+    this.carritoService.agregar(libro);
+  }
 }
