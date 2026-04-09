e2b6ffb28023217fce6e9d53f657b7b8534aa19b
nnova7 <gina.hada@gmail.com>
2026-04-08 21:41:04 -0600

    Se agrego inicio, tanto css como htm como ts

--- frontend/src/app/pages/inicio/inicio.ts
+++ frontend/src/app/pages/inicio/inicio.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio.html',
-  styleUrl: './inicio.css',
+  styleUrl: './inicio.css'
 })
-export class Inicio {
-
-}
+export class Inicio {}
