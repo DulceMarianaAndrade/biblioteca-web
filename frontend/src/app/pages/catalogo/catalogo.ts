a8b99bb2f34234a5fd14eca8ac6df69f0fe1ac2c
Dulce <marianaolvera912@email.com>
2026-04-09 13:59:33 -0600

    Se coloco un filtro para mostrar sólo por categorias

--- frontend/src/app/pages/catalogo/catalogo.ts
+++ frontend/src/app/pages/catalogo/catalogo.ts
@@ -3,10 +3,17 @@ import { RouterLink } from '@angular/router';
 import { CurrencyPipe, UpperCasePipe, TitleCasePipe, NgClass } from '@angular/common';
 import { LibrosService, Libro } from '../../services/libros';
 import { CarritoService } from '../../services/carrito';
+import { CommonModule } from '@angular/common';
+
+
+interface Imagen{
+  url: string;
+  categoria: string;
+}
 
 @Component({
   selector: 'app-catalogo',
-  imports: [RouterLink, CurrencyPipe, UpperCasePipe, TitleCasePipe, NgClass],
+  imports: [RouterLink, CurrencyPipe, UpperCasePipe, TitleCasePipe, NgClass, CommonModule],
   templateUrl: './catalogo.html',
   styleUrl: './catalogo.css'
 })
@@ -14,6 +21,7 @@ export class Catalogo implements OnInit {
   libros = signal<Libro[]>([]);
   cargando = signal(true);
   error = signal('');
+  categoriaSeleccionada = 'todas';
 
   librosService = inject(LibrosService);
   carritoService = inject(CarritoService);
@@ -34,4 +42,56 @@ export class Catalogo implements OnInit {
   agregarAlCarrito(libro: Libro) {
     this.carritoService.agregar(libro);
   }
+
+    imagenes: Imagen[] =[
+    //Novelas
+    {url: 'assets/libro1.jpg', categoria: 'novelas'},
+    {url: 'assets/libro2.jpg', categoria: 'novelas'},
+    {url: 'assets/libro3.jpg', categoria: 'novelas'},
+    {url: 'assets/libro4.jpg', categoria: 'novelas'},
+    {url: 'assets/libro5.jpg', categoria: 'novelas'},
+    {url: 'assets/libro6.jpg', categoria: 'novelas'},
+    {url: 'assets/libro7.jpg', categoria: 'novelas'},
+    {url: 'assets/libro8.jpg', categoria: 'novelas'},
+    {url: 'assets/libro9.jpg', categoria: 'novelas'},
+    {url: 'assets/libro10.jpg', categoria: 'novelas'},
+
+    //Ciencia Ficción
+    {url: 'assets/libro11.jpg', categoria: 'cienciaficcion'},
+    {url: 'assets/libro12.jpg', categoria: 'cienciaficcion'},
+    {url: 'assets/libro13.jpg', categoria: 'cienciaficcion'},
+    {url: 'assets/libro14.jpg', categoria: 'cienciaficcion'},
+    {url: 'assets/libro15.jpg', categoria: 'cienciaficcion'},
+    {url: 'assets/libro16.jpg', categoria: 'cienciaficcion'},
+    {url: 'assets/libro17.jpg', categoria: 'cienciaficcion'},
+    {url: 'assets/libro18.jpg', categoria: 'cienciaficcion'},
+    {url: 'assets/libro19.jpg', categoria: 'cienciaficcion'},
+    {url: 'assets/libro20.jpg', categoria: 'cienciaficcion'},
+    
+    //Superación Personal
+    {url: 'assets/libro21.jpg', categoria: 'SuperacionPersonal'},
+    {url: 'assets/libro22.jpg', categoria: 'SuperacionPersonal'},
+    {url: 'assets/libro23.jpg', categoria: 'SuperacionPersonal'},
+    {url: 'assets/libro24.jpg', categoria: 'SuperacionPersonal'},
+    {url: 'assets/libro25.jpg', categoria: 'SuperacionPersonal'},
+    {url: 'assets/libro26.jpg', categoria: 'SuperacionPersonal'},
+    {url: 'assets/libro27.jpg', categoria: 'SuperacionPersonal'},
+    {url: 'assets/libro28.jpg', categoria: 'SuperacionPersonal'},
+    {url: 'assets/libro29.jpg', categoria: 'SuperacionPersonal'},
+    {url: 'assets/libro30.jpg', categoria: 'SuperacionPersonal'},
+  ];
+
+  filtrarCategoria(categoria: string) {
+    this.categoriaSeleccionada = categoria;
+  }
+
+  librosFiltrados() {
+  if (this.categoriaSeleccionada === 'todas') {
+    return this.libros();
+  }
+
+  return this.libros().filter(libro => 
+    libro.categoria.toLowerCase() === this.categoriaSeleccionada
+  );
+}
 }
