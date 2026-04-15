578502f38d72fdb71b880e2e02c6f1670249ca05
Dulce <marianaolvera912@email.com>
2026-04-14 21:55:33 -0600

    se agrego environment

--- frontend/src/app/services/libros.ts
+++ frontend/src/app/services/libros.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 export interface Libro {
   id: number;
@@ -24,24 +25,24 @@ export interface Mensaje {
 
 @Injectable({ providedIn: 'root' })
 export class LibrosService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'https://librosphere.onrender.com';
 
   constructor(private http: HttpClient) {}
 
   //USO DE SERVICIOS Y HTTPCLIENT: Aqui se definen los metodos para consumir la API REST del backend (GET, POST)
   getLibros(): Observable<Libro[]> { //GET /productos y uso de OBSERVABLES PARA OBTENER LOS LIBROS DESDE EL SERVICIO
-    return this.http.get<Libro[]>(`${this.apiUrl}/api/productos`);
+    return this.http.get<Libro[]>(`${environment.apiUrl}/api/productos`);
   }
 
   getLibroPorId(id: number): Observable<Libro> {// GET /productos/:id
-    return this.http.get<Libro>(`${this.apiUrl}/api/productos/${id}`);
+    return this.http.get<Libro>(`${environment.apiUrl}/api/productos/${id}`);
   }
 
   agregarLibro(libro: any): Observable<any> {// POST /productos
-    return this.http.post(`${this.apiUrl}/api/productos`, libro);
+    return this.http.post(`${environment.apiUrl}/api/productos`, libro);
   }
 
   enviarMensaje(mensaje: Mensaje): Observable<any> {// POST /productos/contacto
-    return this.http.post(`${this.apiUrl}/api/productos/contacto`, mensaje);
+    return this.http.post(`${environment.apiUrl}/api/productos/contacto`, mensaje);
   }
 }
--- frontend/src/app/services/productos.ts
+++ frontend/src/app/services/productos.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 
 export class ProductosService {
-  private API = 'http://localhost:3000/productos';
-  constructor(private http: HttpClient) {}
-  getProductos() {
-    return this.http.get<any[]>(this.API);
+  private apiUrl = environment.apiUrl; 
+  constructor(private http: HttpClient) {} 
+  getProductos() { 
+    return this.http.get<any[]>(`${this.apiUrl}/api/productos`);
   }
 }
