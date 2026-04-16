40833a73775c2a134076b48b611f5c8a2bdf9f6f
Dulce <marianaolvera912@email.com>
2026-04-16 15:45:18 -0600

    Agrego dotenv y configuración de DB

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
+const conexion = mysql.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
